82362be308dc93183d43438f4b056f1473115e9d
Rajat Date <rajatdatey@gmail.com>
2022-07-17 16:03:07 +0530

    [Rajat]:New Test Class Update

---
+++ src/TestClass/Class1.java
@@ -0,0 +1,15 @@
+package TestClass;
+
+public class Class1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Rajat Date");
+		System.out.println("Sushant Bolke");
+		System.out.println("Vidya Chavan");
+		System.out.println("Snehal  Mane");
+		System.out.println("Nikhiraj Karande");
+		
+	}
+
+}
